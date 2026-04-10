24be6c1290f58a873323a6092cb4da51362c9fa7
yaroslav228 <kibersportstandoff69@gmail.com>
2026-04-10 17:25:09 +0300

    first ver

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,30 @@
-function getElementWidth(content, padding, border){
-content = Number.parseFloat(content);
-padding = Number.parseFloat(padding);
-border = Number.parseFloat(border);
 
-return content + padding * 2 + border * 2;
+class StringBuilder{
+    #value;
+
+    constructor(initialValue){
+        this.#value = initialValue;
+    }
+
+    getValue(){
+        return this.#value;
+    }
+    padEnd(str){
+        this.#value = this.#value.concat(str);
+    }
+    padStart(str){
+        this.#value = `${str}${this.#value}`;
+    }
+    padBoth(str){
+        this.#value = `${str}${this.#value}${str}`
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
